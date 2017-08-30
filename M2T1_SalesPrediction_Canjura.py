7738dfd9bc35e960defca7fa7f678b881bb20b48
gmastorg <31484932+gmastorg@users.noreply.github.com>
2017-08-30 13:24:45 -0400

    Add files via upload

---
+++ M2T1_SalesPrediction_Canjura.py
@@ -0,0 +1,10 @@
+#CTI 110
+#M2T1 - Sales Prediction
+#Gabriela Canjura
+#08/30/2017
+#calculate profit margin
+
+totalSales= int(input('Enter total sales: '))
+annualProfit = float(totalSales*.23)
+print ('Annual sales = ',annualProfit,'.')
+
