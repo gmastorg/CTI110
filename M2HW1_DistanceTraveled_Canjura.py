ec3956236b4e87f4b7cc55cee665a5f369b67465
gmastorg <31484932+gmastorg@users.noreply.github.com>
2017-08-30 13:33:35 -0400

    Add files via upload

---
+++ M2HW1_DistanceTraveled_Canjura.py
@@ -0,0 +1,16 @@
+#CTI110
+#M2HW1-Distance Traveled
+#Gabriela Canjura
+#08/30/2017
+#calculates the distance a car traveled after 6,10, and 15 hours
+
+speed= int(70)
+distanceAfter6= int(speed*6)
+distanceAfter10= int(speed*10)
+distanceAfter15= int(speed*15)
+
+print('The car has traveled',distanceAfter6,'miles in 6 hours.')
+print('The car has traveled',distanceAfter10,'miles in 10 hours.')
+print('The car has traveled',distanceAfter15,'miles in 15 hours.')
+
+
